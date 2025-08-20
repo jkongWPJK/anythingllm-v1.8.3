6051c58a82f58f5c92f2e890aa745b7dac9b85ee
Wayne Wang <waynexn@gmail.com>
2025-08-20 02:20:12 -0500

    Modify baseurl to localhsot:3000/llm/

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ const REACTWRAP = isDev ? React.Fragment : React.StrictMode;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <REACTWRAP>
-    <Router>
+    <Router basename="/llm/">
       <App />
     </Router>
   </REACTWRAP>
